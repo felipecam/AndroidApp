4eee855accc77eb3742d599102d44f833106e9a6
felipecam <jfelipecamacho@hotmail.com>
2021-10-20 18:38:56 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fun calculate (){
+            var name = binding.txtname.text
+            Toast.makeText(this,name, Toast.LENGTH_SHORT).show();
+        }
+
+        binding.btnShow.setOnClickListener(){
+            calculate()
+        }
+    }
+}
